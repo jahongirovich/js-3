2a663b7fe70d0bfbb0bb7df9620addce3268bb44
jahongirovich <abdubositabduqahhorov2011@gmail.com>
2025-07-21 10:04:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log(2010)
+console.log("Ismail Jonkahone")
+
+// alert('Привет Абдубосит')
+// alert('Пока')
+
+var name = "Alex"; // старый способ
+let age = 25; // современный, изменяемы й
+const PI = 3.14; // константа, нельзя менять
+
+// camelCase - стандарт
+
+let userName = "Alex";
+let productPrice = 5000;
+
+// новая строка в console.log()
+// console.log("Первая строка \n\nВторая строка");
+
+let user = prompt("String typedagi malumot kiriting");
+// alert("Имя," + user);   
+
+let surname = prompt("Number typedagi malumot kiriting ");
+// alert("Фамиля," + surname); 
+
+let old = prompt("boolean typedagi malumot kiriting");
+// alert("Возраст," + old);
+alert("Hamma malumotlarni to'g'ri kiritganizga ishonchiz komilmi?")
